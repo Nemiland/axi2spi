b44a272ba96488ba571adf7bb9c029f80116e061
Andrew Newman <31496164+Nemiland@users.noreply.github.com>
2020-05-18 10:54:14 -0500

    Fixing dgier

--- axi2spi.srcs/sources_1/new/dgier.vhd
+++ axi2spi.srcs/sources_1/new/dgier.vhd
@@ -22,6 +22,7 @@ ENTITY DGIER IS
 			reg_wack			:	out		std_logic;
 			
 			reg_read_enable		:	in		std_logic;
+			reg_rdata			:	in		std_logic_vector ((C_S_AXI_DATA_WIDTH - 1) downto 0);
 			reg_rack			:	out 	std_logic;
 			
 			--global interrupt enable signal
@@ -37,35 +38,47 @@ END DGIER;
 
 ARCHITECTURE behave OF DGIER IS
 
-SIGNAL dgier_reg : std_logic_vector ((C_S_AXI_DATA_WIDTH - 1) downto 0) := x"00000000";
-
+SIGNAL dgier_reg : std_logic_vector (31 downto 0) := (others => '0');
+SIGNAL masked_wdata : std_logic_vector (C_S_AXI_DATA_WIDTH - 1 downto 0) := (others => '0');
 BEGIN
+
+gi_en <= dgier_reg (31);
 
 PROCESS (reg_clk, reg_rst)
+variable i : integer range 0 to ((C_S_AXI_DATA_WIDTH / 8) - 1) := 0;
 BEGIN
 
-	IF (rising_edge(reg_clk) and dgier_cs = '1') THEN
+	IF (rising_edge(reg_clk) ) THEN
 	
 		IF (reg_rst = '1') THEN
-			dgier_reg <= x"00000000"
-		END IF;
-		
-		IF (reg_read_enable = '1') THEN
-			gi_en <= dgier_reg (31);
-			reg_rack <= '1';
-			wait until rising_edge(reg_clk);
+			dgier_reg <= (others => '0')& X"00000000";
 			reg_rack <= '0';
+			reg_wack <= '0';
+			i := 0;
 		END IF;
 		
-		IF (reg_write_enable = '1') THEN
-			dgier_reg <= reg_wdata;
-			reg_wack <= '1';
-			wait until rising_edge(reg_clk);
+		IF (reg_read_enable = '1' and dgier_cs = '1') THEN
+			reg_rdata <= (others => '0') & dgier_reg; 
+			reg_rack <= not reg_rack;
+			
+		ELSIF (reg_write_enable = '1' and dgier_cs = '1') THEN
+			i := 0;
+			while (i < 4) loop
+				if(reg_wstb(i) = '1') then
+					--update value
+					dgier_reg((i*8 + 7) downto i*8) <= reg_wdata((i*8 + 7) downto i*8);
+				else
+					--keep old value
+					dgier_reg((i*8 + 7) downto i*8) <= dgier_reg((i*8 + 7) downto i*8);
+				end if;
+				i := i + 1;
+			end loop;
+			reg_wack <= not reg_wack;
+		ELSE
+			--no read and no write
+			reg_rack <= '0';
 			reg_wack <= '0';
 		END IF;
-		
-		--error condition???
-		
 	END IF;
 
 END PROCESS;
